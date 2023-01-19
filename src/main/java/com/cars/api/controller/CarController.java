667cd4208df989813f518af9e37df8cecfa50985
luiz <lguilhermefl@gmail.com>
2023-01-19 13:51:32 -0300

    feat: add delete route

--- src/main/java/com/cars/api/controller/CarController.java
+++ src/main/java/com/cars/api/controller/CarController.java
@@ -3,7 +3,9 @@ package com.cars.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cars.api.dto.CarDTO;
 import com.cars.api.model.Car;
 import com.cars.api.repository.CarRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -26,7 +30,12 @@ public class CarController {
     }
 
     @PostMapping
-    public void create(@RequestBody CarDTO req) {
+    public void create(@RequestBody @Valid CarDTO req) {
         repository.save(new Car(req));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
