edaef6b9a10b10022211e4fc3dd089841a434908
luiz <lguilhermefl@gmail.com>
2023-01-19 14:39:43 -0300

    feat: add put route

--- src/main/java/com/cars/api/controller/CarController.java
+++ src/main/java/com/cars/api/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,19 @@ public class CarController {
     @PostMapping
     public void create(@RequestBody @Valid CarDTO req) {
         repository.save(new Car(req));
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody @Valid CarDTO req) {
+        repository.findById(id).map((car) -> {
+            car.setModelo(req.modelo());
+            car.setFabricante(req.fabricante());
+            car.setDataFabricacao(req.dataFabricacao());
+            car.setValor(req.valor());
+            car.setAnoModelo(req.anoModelo());
+
+            return repository.save(car);
+        });
     }
 
     @DeleteMapping("/{id}")
