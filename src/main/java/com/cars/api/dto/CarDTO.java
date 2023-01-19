c893382bdc1981a0b91b913278a267f0d303430f
luiz <lguilhermefl@gmail.com>
2023-01-19 13:51:15 -0300

    feat: add validations

--- src/main/java/com/cars/api/dto/CarDTO.java
+++ src/main/java/com/cars/api/dto/CarDTO.java
@@ -1,6 +1,35 @@
 package com.cars.api.dto;
 
-public record CarDTO(String modelo, String fabricante, String dataFabricacao, double valor, int anoModelo) {
+import java.sql.Date;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
+public record CarDTO(
+
+    @NotBlank
+    String modelo,
+
+    @NotBlank
+    String fabricante, 
+
+    @PastOrPresent
+    Date dataFabricacao,
+
+    @NotNull
+    @Positive
+    double valor,
+
+    @NotNull
+    @Min(1870)
+    @Max(2200)
+    int anoModelo
+
+    ) {
     
 }
 
