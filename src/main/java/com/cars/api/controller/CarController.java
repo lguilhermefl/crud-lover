61798e48ac4cb98eac58980a192b417938b257d2
luiz <lguilhermefl@gmail.com>
2023-01-19 12:54:28 -0300

    init

---
+++ src/main/java/com/cars/api/controller/CarController.java
@@ -0,0 +1,32 @@
+package com.cars.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cars.api.dto.CarDTO;
+import com.cars.api.model.Car;
+import com.cars.api.repository.CarRepository;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarController {
+
+    @Autowired
+    private CarRepository repository;
+
+    @GetMapping
+    public List<Car> listAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public void create(@RequestBody CarDTO req) {
+        repository.save(new Car(req));
+    }
+}
